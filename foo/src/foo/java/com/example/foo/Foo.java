916056a9573e684efe89113ac9cb3658abe1101f
Cedric Champeau <cedric.champeau@gmail.com>
2015-10-21 15:31:20 +0200

    Run the build with a modified Foo reaching out to a dependency of Bar and see a compile failure
    This step...

--- foo/src/foo/java/com/example/foo/Foo.java
+++ foo/src/foo/java/com/example/foo/Foo.java
@@ -1,11 +1,14 @@
 package com.example.foo;
 
 import com.example.bar.Bar;
+import com.google.common.collect.ImmutableList;
 
 public class Foo {
     private final Bar bar;
+    private final ImmutableList<String> items;
 
     public Foo(Bar bar) {
         this.bar = bar;
+        this.items = bar.getItems();
     }
 }
