960fa5928a008686b96c77ed14910d4125f8368d
Cedric Champeau <cedric.champeau@gmail.com>
2015-10-21 15:28:13 +0200

    Add external dependencies to Bar
    This step...

--- bar/src/bar/java/com/example/bar/Bar.java
+++ bar/src/bar/java/com/example/bar/Bar.java
@@ -1,6 +1,10 @@
 package com.example.bar;
 
 import com.example.bar.internal.BarUtils;
+import com.google.common.collect.ImmutableList;
 
 public class Bar {
+   public ImmutableList<String> getItems() {
+	return ImmutableList.of("item1", "item2");
+   }
 }
