425b70e6a6e1d449a41d64000b42c1fa95a15edb
Cedric Champeau <cedric.champeau@gmail.com>
2015-10-21 15:55:11 +0200

    Add a private method to an API class of Bar, recompile, and see that Foo doesn’t need to be rebuilt
    This step...

--- bar/src/bar/java/com/example/bar/Bar.java
+++ bar/src/bar/java/com/example/bar/Bar.java
@@ -5,6 +5,11 @@ import com.google.common.collect.ImmutableList;
 
 public class Bar {
    public ImmutableList<String> getItems() {
-	return ImmutableList.of("item1", "item2", "item3");
+        String[] items = itemsInternal();
+	return ImmutableList.of(items[0], items[1], items[2]);
+   }
+
+   private String[] itemsInternal() {
+	return new String[] { "item1", "item2", "item3" };
    }
 }
